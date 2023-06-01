e42d73fbd4c52891351be72349120d0997b15ef4
ankur <ankursingh91002@gmail.com>
2023-06-01 18:14:15 +0530

    Updates the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import {  useNavigate } from 'react-router-dom';
 import "./Header.css";
 
+
+
 function Header() {
+  const navigate = useNavigate();
+  function handleClick(){
+    navigate("/");
+  }
   return (
     <>
       <div className="header">
-        <h1 className="header-heading">Learn Appwrite</h1>
+        <h1 className="header-heading" onClick={handleClick}>Learn Appwrite</h1>
 
         <a href={"https://github.com/appwrite/appwrite"}>
           <GitHubIcon className="header-button" sx={{ fontSize: 60 }} />
