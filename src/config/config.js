7ce2d598eee1ced99d9319f5ae907c81ba8c3213
ankur <ankursingh91002@gmail.com>
2023-06-09 13:45:16 +0530

    Sets up environment variable and updates the gitignore file

--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,17 @@
 import { Client, Databases, ID, Account } from "appwrite";
-
+// require('dotenv').config();
 const client = new Client();
 const account = new Account(client);
 const databases = new Databases(client);
 
 client
   .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
-  .setProject("647c34a319c46f10741c"); // Your project ID
+  .setProject(process.env.REACT_APP_project_ID); // Your project ID
 
-const Database_ID_v1 = "647c34cda0184d7c964e";
-const Collection_ID_v1 = "647c436a8e7ca3dc98bc";
-const Database_ID_v2 = "647c34cda0184d7c964e";
-const Collection_ID_v2 = "647c34ef6124404ce8d7";
+const Database_ID_v1 = process.env.REACT_APP_Database_ID_v1
+const Collection_ID_v1 = process.env.REACT_APP_Collection_ID_v1;
+const Database_ID_v2 = process.env.REACT_APP_Database_ID_v2;
+const Collection_ID_v2 = process.env.REACT_APP_Collection_ID_v2;
 let randomID = ID.unique();
 
 export {
